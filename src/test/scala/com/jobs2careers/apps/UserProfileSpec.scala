d2c444148fb85da082f9229a01337259b58b45f0
Jacob D. Eisinger <jacob@jobs2careers.com>
2015-07-05 19:37:49 -0500

    use Spark 1.3.1 (AWS EMR version)

--- src/test/scala/com/jobs2careers/apps/UserProfileSpec.scala
+++ src/test/scala/com/jobs2careers/apps/UserProfileSpec.scala
@@ -1,16 +1,16 @@
 package com.jobs2careers.apps
 
-
-import com.jobs2careers.base.{SparkLocalConfig, RedisConfig}
+import com.jobs2careers.base.{ SparkLocalConfig, RedisConfig }
 import com.jobs2careers.utilities.ClassPathResourceLoader
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{DataFrame, SQLContext}
+import org.apache.spark.sql.{ DataFrame, SQLContext }
 import org.scalatest.Matchers._
 import org.scalatest.mock.MockitoSugar
-import org.scalatest.{BeforeAndAfterAll, BeforeAndAfter, FunSpec}
+import org.scalatest.{ BeforeAndAfterAll, BeforeAndAfter, FunSpec }
 import com.redis._
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json.{ JsValue, Json }
+import com.jobs2careers.utilities.SharedSparkContext
 
 /**
  * Example ScalaTest
@@ -27,14 +27,12 @@ import play.api.libs.json.{JsValue, Json}
  * You can run tests by right clicking on the class, or any of the it or
  * describe blocks.
  */
-class UserProfileSpec extends FunSpec with MockitoSugar with BeforeAndAfter with SparkLocalConfig with BeforeAndAfterAll with RedisConfig {
+class UserProfileSpec extends FunSpec with BeforeAndAfter with SharedSparkContext with RedisConfig {
   private val fixture = "fixtures/sample_mail_update.log"
-  private var sc: SparkContext = _
   private var sqlContext: SQLContext = _
   private var mailUpdateDataFrame: DataFrame = _
 
-  override def beforeAll() {
-    sc = new SparkContext(sparkConf)
+  before {
     sqlContext = new SQLContext(sc)
     val resultsBanner =
       """
@@ -67,18 +65,11 @@ class UserProfileSpec extends FunSpec with MockitoSugar with BeforeAndAfter with
       //      println(jobslist)
       //      jobslist should be ("abcdefg")
 
-
     }
   }
 
   // ... any number of it blocks ...
 
-
   // ... any number of describe blocks
-
-
-  override def afterAll() {
-    sc.stop()
-  }
 
 }
