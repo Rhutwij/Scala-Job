a8b69d6e30d37a0b2badc95cf6bfcfbcc98a7170
Jacob D. Eisinger <jacob@jobs2careers.com>
2015-09-10 16:26:41 -0500

    renamed typesafe config and kill SparkContext in JVM for each test

--- src/main/scala/com/jobs2careers/base/RedisConfig.scala
+++ src/main/scala/com/jobs2careers/base/RedisConfig.scala
@@ -6,8 +6,8 @@ import com.typesafe.config.ConfigFactory
  * This trait exposes a Spark Context and the Spark Configuration to anyone
  * mixing it in.
  */
-trait RedisConfig{
-  val conf = ConfigFactory.load()
-  val BIG_DATA_REDIS_DB_HOST = conf.getString("redis.host")
-  val BIG_DATA_REDIS_DB_PORT = conf.getInt("redis.port")
+trait RedisConfig {
+  val applicationConfiguration = ConfigFactory.load()
+  val BIG_DATA_REDIS_DB_HOST = applicationConfiguration.getString("redis.host")
+  val BIG_DATA_REDIS_DB_PORT = applicationConfiguration.getInt("redis.port")
 }
