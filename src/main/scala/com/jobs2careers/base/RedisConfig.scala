d76e6d9d55b384be8a4db97d44b5ea41fe2e467a
Jacob D. Eisinger <jacob@jobs2careers.com>
2015-09-10 15:37:23 -0500

    add redis configuration

--- src/main/scala/com/jobs2careers/base/RedisConfig.scala
+++ src/main/scala/com/jobs2careers/base/RedisConfig.scala
@@ -1,10 +1,13 @@
 package com.jobs2careers.base
 
+import com.typesafe.config.ConfigFactory
+
 /**
  * This trait exposes a Spark Context and the Spark Configuration to anyone
  * mixing it in.
  */
 trait RedisConfig{
-  val BIG_DATA_REDIS_DB_HOST = "profiles-ha.pnuura.ng.0001.use1.cache.amazonaws.com"
-  val BIG_DATA_REDIS_DB_PORT = 6379
+  val conf = ConfigFactory.load()
+  val BIG_DATA_REDIS_DB_HOST = conf.getString("redis.host")
+  val BIG_DATA_REDIS_DB_PORT = conf.getInt("redis.port")
 }
